1b4e66c0fdec34e6ee5d94184f24619823a8a1fe
luisGomez <luis.gomez.veloz@gmail.com>
2024-09-19 08:29:32 -0500

    New design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,61 @@ export default {
 			},
 			boxShadow: {
 				'3xl': '5px 5px 10px 0px rgba(0,0,0,0.15)',
-			}
+			},
+			colors: {
+				primary: {
+					light: '#415f91',
+					dark: '#aac7ff',
+				},
+				secondary: {
+					light: '#565f71',
+					dark: '#bec6dc',
+				},
+				tertiary: {
+					light: '#79590c',
+					dark: '#ebc16c',
+				},
+				primaryContainer: {
+					light: '#d6e3ff',
+					dark: '#284777',
+				},
+				secondaryContainer: {
+					light: '#dae2f9',
+					dark: '#3e4759',
+				},
+				tertiaryContainer: {
+					light: '#ffdea3',
+					dark: '#5d4200',
+				},
+				surface: {
+					light: '#f9f9ff',
+					dark: '#111318',
+				},
+				surfaceContainer: {
+					light: '#ededf4',
+					dark: '#1d2024',
+				},
+				onSurface: {
+					light: '#191c20',
+					dark: '#e2e2e9',
+				},
+				danger: {
+					light: '#ba1a1a',
+					dark: '#ffb4ab',
+				},
+				dangerContainer: {
+					light: '#ffdad6',
+					dark: '#93000a',
+				},
+				outline: {
+					light: '#74777f',
+					dark: '#8e9099',
+				},
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+				suse: ['SUSE', 'sans-serif']
+			},
 		},
 	},
 	plugins: [
